0449be912dec39fe71f0961d843e2c14fb10c7f4
Agung Wahyudin <EMAIL_GITHUB_agungwahyudin510>
2026-06-01 11:36:38 +0700

    fix: use next.config.js with ducanh2912/next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
+import withPWA from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withPWA(nextConfig);
+export default withPWA({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+})(nextConfig);
